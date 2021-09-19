fb28ed2880494df9c3d4a93d1e1913d465ccdd7f
Stas <E:\Private\SoftData\TheBAT\MyMail>
2021-09-19 14:36:16 +0300

    Strict paths

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ results_dir=`pwd`/build-results
 ! rm -rf "$results_dir" >/dev/null 2>&1
 mkdir "$results_dir"
 
-# docker build -t $container_name "$script_dir/docker"
-# docker run -it --rm -v "$results_dir:/root/build-results" --name $container_name-instance $container_name
+docker build -t $container_name "$script_dir/docker"
+docker run -it --rm -v "$results_dir:/root/build-results" --name $container_name-instance $container_name
 
 echo "Done. You can find windows binaries here: $results_dir"
