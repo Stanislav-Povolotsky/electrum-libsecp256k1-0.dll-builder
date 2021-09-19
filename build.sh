c73140af775df817adaa14c66208a4d9a858b825
Stas <E:\Private\SoftData\TheBAT\MyMail>
2021-09-19 14:29:15 +0300

    Strict paths

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
-
 container_name=electrum-builder
+script_dir=$(dirname "$(readlink -f "$0")")
+results_dir=`pwd`/build-results
 
 ! docker stop $container_name-instance
 ! docker kill --signal=9 $container_name-instance
-! rm -rf ./build-results >/dev/null 2>&1
+! rm -rf "$results_dir" >/dev/null 2>&1
+mkdir "$results_dir"
 
-mkdir ./build-results
+# docker build -t $container_name "$script_dir/docker"
+# docker run -it --rm -v "$results_dir:/root/build-results" --name $container_name-instance $container_name
 
-docker build -t $container_name ./docker
-docker run -it --rm -v `pwd`/build-results:/root/build-results --name $container_name-instance $container_name
+echo "Done. You can find windows binaries here: $results_dir"
